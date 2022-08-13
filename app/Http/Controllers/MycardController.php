66ec1edf232d2ae9d2a1429ae5312478853666f6
hazman1yusof <your-email@xxx.comhazman.yusof@gmail.com>
2022-08-13 22:38:57 +0800

    ssdsd

--- app/Http/Controllers/MycardController.php
+++ app/Http/Controllers/MycardController.php
@@ -19,15 +19,16 @@ class MycardController extends Controller
 
     }
 
-    public function get_data(Request $request)
+    public function read_mykad(Request $request)
     {   
-
 
         exec(storage_path('app\mykad\mykad.exe'), $output, $retval);
         
         if(!empty($output)){
 
             $array_mycard = explode("|", $output[0]);
+
+            // $this->save_to_file_mykad($request->path,$array_mycard);
 
             if($array_mycard[0] == 'Smart card reader fail to connect.'){
                 $responce = new stdClass();
@@ -62,56 +63,75 @@ class MycardController extends Controller
             $responce->reason = 'Other';
             return json_encode($responce);
         }
+    }
 
-        // dd(storage_path('app\mykad32bit\mykad.exe'));
-        // $process = new Process('C:\laragon\www\msoftweb\app\Http\Controllers\util\runbarcode.bat');
-    
-        // // executes after the command finishes
-        // if ($process->isSuccessful()) {
+    public function read_mykid(Request $request)
+    {   
 
-        //     // throw new ProcessFailedException($process);
-        //     $contents = File::get(storage_path('app\mykad32bit\mykad.txt'));
 
-        //     $array_mycard = explode("|",$contents);
-        //     if(empty($array_mycard[1])){
-                
-        //         $responce = new stdClass();
-        //         $responce->status = 'failed';
-        //         $responce->reason = $array_mycard[0];
-        //     }else{
-                
-        //         $responce = new stdClass();
-        //         $responce->status = 'success';
-        //         $responce->ic = $array_mycard[0];
-        //         $responce->dob = $array_mycard[1];
-        //         $responce->birthplace = $array_mycard[2];
-        //         $responce->name = $array_mycard[3];
-        //         $responce->oldic = $array_mycard[4];
-        //         $responce->religion = $array_mycard[5];
-        //         $responce->sex = $array_mycard[6];
-        //         $responce->race = $array_mycard[7];
-        //         $responce->addr1 = $array_mycard[8];
-        //         $responce->addr2 = $array_mycard[9];
-        //         $responce->addr3 = $array_mycard[10];
-        //         $responce->postcode = $array_mycard[11];
-        //         $responce->city = $array_mycard[12];
-        //         $responce->state = $array_mycard[13];
+        exec(storage_path('app\mykid\mykid.exe'), $output, $retval);
+        
+        if(!empty($output)){
 
-        //         $img = Image::make(storage_path('app\mykad32bit\myphotov1.jpg'));
+            $array_mycard = explode("|", $output[0]);
 
-        //         // $file = File::get(storage_path('app\mykad32bit\myphotov1.jpg'));
-        //         // dd((string) $img->encode('data-url'));
-        //         $responce->mykad_photo = (string) $img->encode('data-url');
-        //     }
-        // }else{
+            // $this->save_to_file_mykid($request->path,$array_mycard);
 
-        //     // throw new ProcessFailedException($process);
-        //     $responce = new stdClass();
-        //     $responce->status = 'failed';
-        //     $responce->reason = 'Other';
-        // }
+            if($array_mycard[0] == 'Smart card reader fail to connect.'){
+                $responce = new stdClass();
+                $responce->status = 'failed';
+                $responce->reason = 'Smart card reader fail to connect.';
+                return json_encode($responce);
+            }
 
-        // return json_encode($responce);
+            $responce = new stdClass();
+            $responce->status = 'success';
+            $responce->ic = $array_mycard[0];
+            $responce->dob = $array_mycard[1];
+            $responce->birthplace = $array_mycard[2];
+            $responce->name = $array_mycard[3];
+            $responce->religion = $array_mycard[5];
+            $responce->sex = $array_mycard[6];
+            $responce->race = $array_mycard[7];
+            $responce->addr1 = $array_mycard[8];
+            $responce->addr2 = $array_mycard[9];
+            $responce->addr3 = $array_mycard[10];
+            $responce->postcode = $array_mycard[11];
+            $responce->city = $array_mycard[12];
+            $responce->state = $array_mycard[13];
+            return json_encode($responce);
+
+
+        }else{
+            $responce = new stdClass();
+            $responce->status = 'failed';
+            $responce->reason = 'Other';
+            return json_encode($responce);
+        }
+    }
+
+    public function save_to_file_mykad($path,$array_mycard){
+        $myfile = fopen($path."\mykad\mykad.txt", "a") or die("Unable to open file!");
+        $text=$array_mycard[0];
+        foreach ($array_mycard as $key => $value) {
+            if($key<14 && $key>0){
+                $text = $text .'|'. $value;
+            }
+        }
+        fwrite($myfile, "\n".$text);
+        fclose($myfile);
+    }
+
+    public function save_to_file_mykid($path,$array_mycard){
+        $myfile = fopen($path."\mykad\mykid.txt", "a") or die("Unable to open file!");
+        $text=$array_mycard[0];
+        foreach ($array_mycard as $key => $value) {
+            if($key>0){
+                $text = $text .'|'. $value;
+            }
+        }
+        fwrite($myfile, "\n".$text);
+        fclose($myfile);
     }
 
 }
